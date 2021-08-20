0b6d14662b23dc6d7bd933f863394fc54c723e4e
Dominique Orban <dominique.orban@gmail.com>
2021-08-20 16:32:30 -0400

    improve docstrings

--- src/bpdn_model.jl
+++ src/bpdn_model.jl
@@ -14,6 +14,36 @@ end
 
 bpdn_data(compound :: Int = 1, args...) = bpdn_data(200 * compound, 512 * compound, 10 * compound, args...)
 
+"""
+    model, sol = bpdn_model(args...)
+    model, sol = bpdn_model(compound = 1, args...)
+
+Return an instance of an `NLPModel` representing the basis-pursuit denoise
+problem, i.e., the under-determined linear least-squares objective
+
+   ½ ‖Ax - b‖₂²,
+
+where A has orthonormal rows and b = A * x̄ + ϵ, x̄ is sparse and ϵ is a noise
+vector following a normal distribution with mean zero and standard deviation σ.
+
+## Arguments
+
+* `m :: Int`: the number of rows of A
+* `n :: Int`: the number of columns of A (with `n` ≥ `m`)
+* `k :: Int`: the number of nonzero elements in x̄
+* `noise :: Float64`: noise amount ϵ (default: 0.01).
+
+The second form calls the first form with arguments
+
+    m = 200 * compound
+    n = 512 * compound
+    k =  10 * compound
+
+## Return Value
+
+An instance of a `FirstOrderModel` that represents the basis-pursuit denoise problem
+and the exact solution x̄.
+"""
 function bpdn_model(args...)
   A, b, b0, x0 = bpdn_data(args...)
   r = similar(b)
@@ -38,6 +68,16 @@ function bpdn_model(args...)
   FirstOrderModel(obj, grad!, zero(x0), name = "BPDN"), x0
 end
 
+"""
+    model, sol = bpdn_nls_model(args...)
+    model, sol = bpdn_nls_model(compound = 1, args...)
+
+Return an instance of a `FirstOrderNLSModel` that represents the basis-pursuit
+denoise problem explicitly as a least-squares problem and the exact solution x̄.
+
+See the documentation of `bpdn_model()` for more information and a
+description of the arguments.
+"""
 function bpdn_nls_model(args...)
   A, b, b0, x0 = bpdn_data(args...)
   r = similar(b)
