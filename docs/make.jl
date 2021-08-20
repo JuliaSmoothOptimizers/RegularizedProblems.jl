0b65cc411aa5a495e0c022484d06b3b2306c79e9
Dominique Orban <dominique.orban@gmail.com>
2021-08-20 16:33:37 -0400

    add docs

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter
+using ADNLPModels, DifferentialEquations
+using RegularizedProblems
+
+makedocs(
+  modules = [RegularizedProblems],
+  doctest = true,
+  # linkcheck = true,
+  strict = true,
+  format = Documenter.HTML(
+    assets = ["assets/style.css"],
+    prettyurls = get(ENV, "CI", nothing) == "true",
+  ),
+  sitename = "RegularizedProblems.jl",
+  pages = Any["Home" => "index.md", "Reference" => "reference.md"],
+)
+
+deploydocs(repo = "github.com/JuliaSmoothOptimizers/RegularizedProblems.jl.git")
