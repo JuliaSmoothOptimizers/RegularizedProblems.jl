85893b576d1c84175fad579b9f24612287cb4d75
rjbaraldi <rbaraldi@uw.edu>
2022-08-26 14:31:06 -0700

    updates to svm

--- src/nonlin_svm_model.jl
+++ src/nonlin_svm_model.jl
@@ -3,6 +3,17 @@ export tanh_train_model, tanh_test_model#, tan_nls_model
 function tan_data_train()
   #load data
   A, b = MLDatasets.MNIST.traindata()
+  A, b = generate_data(A,b)
+  return A,b
+end
+
+function tan_data_test()
+  A0, b0 = MLDatasets.MNIST.testdata()
+  A0, b0 = generate_data(A,b)
+  return A0, b0
+end
+
+function generate_data(A,b, switch=true)
   ind = findall(x -> x == 0 || x == 1, b)
   #reshape to matrix
   A = reshape(A,size(A,1)*size(A,2), size(A,3))./255
@@ -11,22 +22,12 @@ function tan_data_train()
   b = b[ind]
   b[b.==0] .= -1
   A = convert(Array{Float64, 2}, A[:, ind])
-  p = randperm(length(b))[1:Int(floor(length(b)/4))]
-  b = b[p]
-  A = A[:, p]
+  if switch
+    p = randperm(length(b))[1:Int(floor(length(b)/4))]
+    b = b[p]
+    A = A[:, p]
+  end
   return A, b
-end
-
-function tan_data_test()
-  A0, b0 = MLDatasets.MNIST.testdata()
-  ind = findall(x -> x == 0 || x == 1, b)
-  A0 = reshape(A0,size(A0,1)*size(A0,2), size(A0,3))./255
-
-  #get 0s and 1s
-  b0 = b0[ind]
-  b0[b0.==0] .= -1
-  A0 = convert(Array{Float64, 2}, A0[:, ind])
-  return A0, b0
 end
 
 """
@@ -55,14 +56,10 @@ With the MNIST Dataset, the dimensions are:
 An instance of a `FirstOrderModel` that represents the complete SVM problem in NLP form, and
 an instance of `FirstOrderNLSModel` that represents the nonlinear least squares in nonlinear least squares form.
 """
-function svm_train_model(args...)
-  if length(args)<1
-    A, b = tan_data_train()
-  elseif length(args) == 1 || length(args) > 2
-    error("Must supply data matrix A and labels b!")
-  else
-    (A,b) = args
-  end
+
+svm_train_model() = svm_train_model(tan_data_train()...)
+
+function svm_train_model(A, b)
   Ahat = Diagonal(b)*A'
   r = zeros(size(Ahat,1))
   tmp = similar(r)
@@ -106,14 +103,8 @@ function svm_train_model(args...)
   FirstOrderModel(obj, grad!, ones(size(A,1)), name = "Nonlinear-SVM"), FirstOrderNLSModel(resid!, jacv!, jactv!, size(b,1), ones(size(A,1))), b
 end
 
+svm_test_model() = svm_test_model(tan_data_test()...)
 function svm_test_model()
-  if length(args)<1
-    A, b = tan_data_test()
-  elseif length(args) == 1 || length(args) > 2
-    error("Must supply data matrix A and labels b!")
-  else
-    (A,b) = args
-  end
   Ahat = Diagonal(b)*A'
   r = zeros(size(Ahat,1))
   tmp = similar(r)
