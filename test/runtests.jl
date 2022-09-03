246adb227be0037f6ba6a2d672da197749747426
Dominique <dominique.orban@gmail.com>
2022-09-03 13:50:04 -0700

    Add matrix completion problems (#19)
    Co-authored-by: MohamedLaghdafHABIBOULLAH <81633807+MohamedLaghdafHABIBOULLAH@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -47,3 +47,69 @@ end
   JtF = jtprod_residual(nls_model, x, F)
   @test all(g .≈ JtF)
 end
+
+@testset "low-rank completion" begin
+  model, nls_model, A = lrcomp_model(100, 100)
+  @test typeof(model) <: FirstOrderModel
+  @test typeof(A) == Vector{Float64}
+  @test model.meta.nvar == 10000
+  @test all(0 .<= model.meta.x0 .<= 1)
+
+  @test typeof(nls_model) <: FirstOrderNLSModel
+  @test nls_model.meta.nvar == 10000
+  @test nls_model.nls_meta.nequ == 10000
+  @test all(0 .<= nls_model.meta.x0 .<= 1)
+
+  x = fill!(similar(model.meta.x0), 1)
+  f = obj(model, x)
+  F = residual(nls_model, x)
+  @test f ≈ dot(F, F) / 2
+
+  g = grad(model, x)
+  JtF = jtprod_residual(nls_model, x, F)
+  @test all(g .≈ JtF)
+end
+
+@testset "mat_rand" begin
+  model, nls_model, sol = random_matrix_completion_model(100, 100, 5, 0.8, 0.0001, 0.01, 0.2)
+  @test typeof(model) <: FirstOrderModel
+  @test typeof(sol) == Matrix{Float64}
+  @test model.meta.nvar == 10000
+  @test all(0 .<= model.meta.x0 .<= 1)
+
+  @test typeof(nls_model) <: FirstOrderNLSModel
+  @test nls_model.meta.nvar == 10000
+  @test nls_model.nls_meta.nequ == 10000
+  @test all(0 .<= nls_model.meta.x0 .<= 1)
+
+  x = fill!(similar(model.meta.x0), 1)
+  f = obj(model, x)
+  F = residual(nls_model, x)
+  @test f ≈ dot(F, F) / 2
+
+  g = grad(model, x)
+  JtF = jtprod_residual(nls_model, x, F)
+  @test all(g .≈ JtF)
+end
+
+@testset "MIT" begin
+  model, nls_model, sol = MIT_matrix_completion_model()
+  @test typeof(model) <: FirstOrderModel
+  @test typeof(sol) == Matrix{Float64}
+  @test model.meta.nvar == 256 * 256
+  @test all(0 .<= model.meta.x0 .<= 1)
+
+  @test typeof(nls_model) <: FirstOrderNLSModel
+  @test nls_model.meta.nvar == 256 * 256
+  @test nls_model.nls_meta.nequ == 256 * 256
+  @test all(0 .<= nls_model.meta.x0 .<= 1)
+
+  x = fill!(similar(model.meta.x0), 1)
+  f = obj(model, x)
+  F = residual(nls_model, x)
+  @test f ≈ dot(F, F) / 2
+
+  g = grad(model, x)
+  JtF = jtprod_residual(nls_model, x, F)
+  @test all(g .≈ JtF)
+end
