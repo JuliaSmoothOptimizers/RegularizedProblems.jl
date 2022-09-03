e9c7404e6da8eadf930e42bdcfff87166bbec2e0
Dominique Orban <dominique.orban@gmail.com>
2022-09-03 19:08:56 -0400

    add support for selected variables

--- src/types.jl
+++ src/types.jl
@@ -14,28 +14,48 @@ A simple subtype of `AbstractNLPModel` to represent a smooth objective.
 
 ## Keyword arguments
 
-All keyword arguments are passed through to the `NLPModelMeta` constructor.
+* `selected :: AbstractVector{<: Int}`: a list of variables to apply the regularizer to
+    (default: all variables).
+
+All other keyword arguments are passed through to the `NLPModelMeta` constructor.
 """
-mutable struct FirstOrderModel{T, S, F, G} <: AbstractNLPModel{T, S}
+mutable struct FirstOrderModel{T, S, F, G, I, V} <: AbstractNLPModel{T, S}
   meta::NLPModelMeta{T, S}
   counters::Counters
 
   f::F
   ∇f!::G
+  selected::V
 
-  function FirstOrderModel{T, S, F, G}(
+  function FirstOrderModel{T, S, F, G, I, V}(
     f::F,
     ∇f!::G,
     x::S;
+    selected::AbstractVector{I} = 1:length(x),
     kwargs...,
-  ) where {T, S, F <: Function, G <: Function}
-    meta = NLPModelMeta(length(x), x0 = x; kwargs...)
-    return new{T, S, F, G}(meta, Counters(), f, ∇f!)
+  ) where {T, S, F <: Function, G <: Function, I <: Integer, V <: AbstractVector{I}}
+    nvar = length(x)
+    slct = unique(selected)
+    @assert all(1 .≤ slct .≤ nvar)
+    meta = NLPModelMeta(nvar, x0 = x; kwargs...)
+    return new{T, S, F, G, I, typeof(slct)}(meta, Counters(), f, ∇f!, slct)
   end
 end
 
-FirstOrderModel(f, ∇f!, x::S; kwargs...) where {S} =
-  FirstOrderModel{eltype(S), S, typeof(f), typeof(∇f!)}(f, ∇f!, x; kwargs...)
+FirstOrderModel(
+  f,
+  ∇f!,
+  x::S;
+  selected::V = 1:length(x),
+  kwargs...,
+) where {S, I <: Integer, V <: AbstractVector{I}} =
+  FirstOrderModel{eltype(S), S, typeof(f), typeof(∇f!), I, V}(
+    f,
+    ∇f!,
+    x,
+    selected = selected;
+    kwargs...,
+  )
 
 function NLPModels.obj(nlp::FirstOrderModel, x::AbstractVector)
   NLPModels.@lencheck nlp.meta.nvar x
@@ -65,9 +85,12 @@ with a smooth residual.
 
 ## Keyword arguments
 
-All keyword arguments are passed through to the `NLPModelMeta` constructor.
+* `selected :: AbstractVector{<: Int}`: a list of variables to apply the regularizer to
+    (default: all variables).
+
+All other keyword arguments are passed through to the `NLPModelMeta` constructor.
 """
-mutable struct FirstOrderNLSModel{T, S, R, J, Jt} <: AbstractNLSModel{T, S}
+mutable struct FirstOrderNLSModel{T, S, R, J, Jt, I, V} <: AbstractNLSModel{T, S}
   meta::NLPModelMeta{T, S}
   nls_meta::NLSMeta{T, S}
   counters::NLSCounters
@@ -75,28 +98,42 @@ mutable struct FirstOrderNLSModel{T, S, R, J, Jt} <: AbstractNLSModel{T, S}
   resid!::R
   jprod_resid!::J
   jtprod_resid!::Jt
+  selected::V
 
-  function FirstOrderNLSModel{T, S, R, J, Jt}(
+  function FirstOrderNLSModel{T, S, R, J, Jt, I, V}(
     r::R,
     jv::J,
     jtv::Jt,
     nequ::Int,
     x::S;
+    selected::AbstractVector{I} = 1:length(x),
     kwargs...,
-  ) where {T, S, R <: Function, J <: Function, Jt <: Function}
-    meta = NLPModelMeta(length(x), x0 = x; kwargs...)
-    nls_meta = NLSMeta{T, S}(nequ, length(x), x0 = x)
-    return new{T, S, R, J, Jt}(meta, nls_meta, NLSCounters(), r, jv, jtv)
+  ) where {T, S, R <: Function, J <: Function, Jt <: Function, I <: Integer, V <: AbstractVector{I}}
+    nvar = length(x)
+    slct = unique(selected)
+    @assert all(1 .≤ slct .≤ nvar)
+    meta = NLPModelMeta(nvar, x0 = x; kwargs...)
+    nls_meta = NLSMeta{T, S}(nequ, nvar, x0 = x)
+    return new{T, S, R, J, Jt, I, typeof(slct)}(meta, nls_meta, NLSCounters(), r, jv, jtv, slct)
   end
 end
 
-FirstOrderNLSModel(r, jv, jtv, nequ::Int, x::S; kwargs...) where {S} =
-  FirstOrderNLSModel{eltype(S), S, typeof(r), typeof(jv), typeof(jtv)}(
+FirstOrderNLSModel(
+  r,
+  jv,
+  jtv,
+  nequ::Int,
+  x::S;
+  selected::V = 1:length(x),
+  kwargs...,
+) where {S, I <: Integer, V <: AbstractVector{I}} =
+  FirstOrderNLSModel{eltype(S), S, typeof(r), typeof(jv), typeof(jtv), I, V}(
     r,
     jv,
     jtv,
     nequ,
-    x;
+    x,
+    selected = selected;
     kwargs...,
   )
 
