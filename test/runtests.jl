fadf393a6b57086062a883f79105c3bbc3ab6a8c
Dominique Orban <dominique.orban@gmail.com>
2022-09-03 12:29:59 -0400

    unit tests for low-rank completion problem
    code courtesy of @MohamedLaghdafHABIBOULLAH.

--- test/runtests.jl
+++ test/runtests.jl
@@ -47,3 +47,25 @@ end
   JtF = jtprod_residual(nls_model, x, F)
   @test all(g .≈ JtF)
 end
+
+@testset "low-rank completion" begin
+  model, nls_model, A = lrcomp_model(100, 100)
+  @test typeof(model) <: FirstOrderModel
+  @test typeof(A) == Vector{Float64}
+  @test model.meta.nvar == 10000
+  @test all(0 .<= model.meta.x0 .<= 1)
+
+  @test typeof(nls_model) <: FirstOrderNLSModel
+  @test nls_model.meta.nvar == 10000
+  @test nls_model.nls_meta.nequ == 10000
+  @test all(0 .<= nls_model.meta.x0 .<= 1)
+
+  x = fill!(similar(model.meta.x0), 1)
+  f = obj(model, x)
+  F = residual(nls_model, x)
+  @test f ≈ dot(F, F) / 2
+
+  g = grad(model, x)
+  JtF = jtprod_residual(nls_model, x, F)
+  @test all(g .≈ JtF)
+end
