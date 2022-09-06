38223a9f5bd23e3825c4113414c7feb1ace93dc4
rjbaraldi <rbaraldi@uw.edu>
2022-09-05 22:58:29 -0700

    updated RegProb file, group lasso to format, unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,6 +30,19 @@ end
   @test nls_model.nls_meta.nequ == 200
 end
 
+@testset "Group-BPDN" begin
+  model, nls_model, sol, g, ag, ind = group_lasso_model()
+  test_well_defined(model, nls_model, sol)
+  test_objectives(model, nls_model)
+  @test model.meta.nvar == 512
+  @test all(model.meta.x0 .== 0)
+  @test length(findall(x -> x .!= 0, sol))/length(ag) == 32
+  @test nls_model.nls_meta.nequ == 200
+  @test g == 16
+  @test length(ag) == 5
+  @test size(ind) == (16, 32)
+end
+
 @testset "BPDN with bounds" begin
   model, nls_model, sol = bpdn_model(; bounds = true)
   test_well_defined(model, nls_model, sol)
