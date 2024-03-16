6818d016aee172b3c8649a7e1dda90e5cc9c6d1e
dpo <dpo@users.noreply.github.com>
2024-03-16 22:00:44 +0000

    :robot: Format .jl files

--- test/runtests.jl
+++ test/runtests.jl
@@ -155,12 +155,12 @@ end
 @testset "QP-rand" begin
   n, dens = 100, 0.1
   model = qp_rand_model(n; dens = dens, convex = false)
-  @test all(-2.0 .≤  model.meta.lvar .≤ 0.0)
-  @test all(0.0 .≤  model.meta.uvar .≤ 2.0)
+  @test all(-2.0 .≤ model.meta.lvar .≤ 0.0)
+  @test all(0.0 .≤ model.meta.uvar .≤ 2.0)
   @test all(model.meta.x0 .== 0)
 
   model = qp_rand_model(n; dens = dens, convex = true)
-  @test all(-2.0 .≤  model.meta.lvar .≤ 0.0)
-  @test all(0.0 .≤  model.meta.uvar .≤ 2.0)
+  @test all(-2.0 .≤ model.meta.lvar .≤ 0.0)
+  @test all(0.0 .≤ model.meta.uvar .≤ 2.0)
   @test all(model.meta.x0 .== 0)
 end
