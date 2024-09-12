25e9997e6b6fb2fb08cb34314d5e5b3d22bd41f0
MohamedLaghdafHABIBOULLAH <mohamed-laghdaf-2.habiboullah@polymtl.ca>
2024-09-12 14:37:40 -0400

    Add denoising problem

--- src/denoising_data.jl
+++ src/denoising_data.jl
@@ -1,4 +1,21 @@
-export generate_HW
+export generate_uniform_blur, generate_gaussian_blur
+
+"""
+Implementation of the denoising problem described in 
+
+Stella, L., Themelis, A. & Patrinos, P. 
+Forward–backward quasi-Newton methods for nonsmooth optimization problems. 
+Comput Optim Appl 67, 443–487 (2017). https://doi.org/10.1007/s10589-017-9912-y
+
+and adapted from the original implementation in Python by the authors of the paper:
+
+Chouzenoux, E., Martin, S. & Pesquet, JC. 
+A Local MM Subspace Method for Solving Constrained Variational Problems in Image Recovery. 
+J Math Imaging Vis 65, 253–276 (2023). https://doi.org/10.1007/s10851-022-01112-z
+"""
+# non-allocating reshape
+# see https://github.com/JuliaLang/julia/issues/36313
+reshape_array(a, dims) = invoke(Base._reshape, Tuple{AbstractArray, typeof(dims)}, a, dims)
 
 # Function to unpad an array
 function unpad(x, n_p, m_p, n)
@@ -24,7 +41,7 @@ function pad_x(z, a)
   return t
 end
 
-# Function to create a meshgrid for the kernel
+# Function to create a meshgrid for the gaussian kernel
 function meshgrid(x_range, y_range)
   m = length(x_range)
   n = length(y_range)
@@ -36,9 +53,9 @@ end
 # Function to generate a Gaussian kernel
 function my_gaussian_kernel(kernel_size, kernel_sigma)
   x, y = meshgrid((-kernel_size):kernel_size, (-kernel_size):kernel_size)
-  normal = 1 / (2.0 * pi * kernel_sigma^2)
-  kernel = exp.(-((x .^ 2 + y .^ 2) / (2.0 * kernel_sigma^2))) * normal
-  kernel = kernel / sum(kernel)
+  normal = 1 / (2 * pi * kernel_sigma^2)
+  kernel = exp(-((x ^ 2 + y ^ 2) / (2 * kernel_sigma^2))) * normal
+  kernel ./= sum(kernel)
   return kernel
 end
 
@@ -49,31 +66,25 @@ function uniform_kernel(kernel_size)
   return kernel
 end
 
-# Main function to generate H, H_T, W, and W_T functions
-function generate_HW(shape, shape_p, KERNEL_SIZE, KERNEL_TYPE, KERNEL_SIGMA = 1.5)
+# Main function to generate H, H_T, W, and W_T functions for gaussian kernel
+function generate_gaussian_blur(shape, shape_p, KERNEL_SIZE, KERNEL_SIGMA = 1.5)
   (n, m) = shape
   (n_p, m_p) = shape_p
   a = n_p - n
-
-  if KERNEL_TYPE == "gaussian"
-    kernel_h = my_gaussian_kernel(KERNEL_SIZE, KERNEL_SIGMA)
-  elseif KERNEL_TYPE == "uniform"
-    kernel_h = uniform_kernel(KERNEL_SIZE)
-  else
-    println("This KERNEL_TYPE is not defined")
-  end
+  
+  kernel_h = my_gaussian_kernel(KERNEL_SIZE, KERNEL_SIGMA)
 
   sz = (n_p - (2 * KERNEL_SIZE + 1), m_p - (2 * KERNEL_SIZE + 1))
   kernel_h = pad(kernel_h, div(sz[1], 2), div(sz[1], 2) + 1)
-  kernel_h = ifftshift(kernel_h)
-  fft_h = fft(kernel_h)
+  kernel_h = FFTW.ifftshift(kernel_h)
+  fft_h = FFTW.fft(kernel_h)
 
   # Function H: Applies a linear transformation H which models the blur to an input x
   function H(x)
     x = reshape_array(x, (n, m))
     x = pad_x(x, a)
-    fft_x = fft(x)
-    x_new = real(ifft(fft_x .* fft_h))
+    fft_x = FFTW.fft(x)
+    x_new = real(FFTW.ifft(fft_x .* fft_h))
     return unpad(x_new, n_p, m_p, n)[:]
   end
 
@@ -81,23 +92,72 @@ function generate_HW(shape, shape_p, KERNEL_SIZE, KERNEL_TYPE, KERNEL_SIGMA = 1.
   function H_T(x)
     x = reshape_array(x, (n, m))
     x = pad_x(x, a)
-    fft_x = fft(x)
-    x_new = real(ifft(fft_x .* conj.(fft_h)))
+    fft_x = FFTW.fft(x)
+    x_new = real(FFTW.ifft(fft_x .* conj.(fft_h)))
     return unpad(x_new, n_p, m_p, n)[:]
   end
 
-  wt = wavelet(WT.haar)
+  wt = Wavelets.wavelet(Wavelets.WT.haar)
 
   # ----- Discrete Wavelet Transform (DWT) -----
 
   # Function W: Applies the DWT to an input x
   function W(x)
-    return dwt(reshape_array(x, (n, m)), wt, 4)[:]
+    return Wavelets.dwt(reshape_array(x, (n, m)), wt, 4)[:]
   end
 
   # Function W_T: Applies the inverse DWT to an input x
   function W_T(x)
-    return idwt(reshape_array(x, (n, m)), wt, 4)[:]
+    return Wavelets.idwt(reshape_array(x, (n, m)), wt, 4)[:]
+  end
+
+  # Return the generated functions
+  return H, H_T, W, W_T
+end
+
+# Main function to generate H, H_T, W, and W_T functions for gaussian kernel
+function generate_uniform_blur(shape, shape_p, KERNEL_SIZE)
+  (n, m) = shape
+  (n_p, m_p) = shape_p
+  a = n_p - n
+
+  kernel_h = uniform_kernel(KERNEL_SIZE)
+  
+  sz = (n_p - (2 * KERNEL_SIZE + 1), m_p - (2 * KERNEL_SIZE + 1))
+  kernel_h = pad(kernel_h, div(sz[1], 2), div(sz[1], 2) + 1)
+  kernel_h = FFTW.ifftshift(kernel_h)
+  fft_h = FFTW.fft(kernel_h)
+
+  # Function H: Applies a linear transformation H which models the blur to an input x
+  function H(x)
+    x = reshape_array(x, (n, m))
+    x = pad_x(x, a)
+    fft_x = FFTW.fft(x)
+    x_new = real(FFTW.ifft(fft_x .* fft_h))
+    return unpad(x_new, n_p, m_p, n)[:]
+  end
+
+  # Function H_T: Applies the transpose of the linear transformation H to an input x
+  function H_T(x)
+    x = reshape_array(x, (n, m))
+    x = pad_x(x, a)
+    fft_x = FFTW.fft(x)
+    x_new = real(FFTW.ifft(fft_x .* conj.(fft_h)))
+    return unpad(x_new, n_p, m_p, n)[:]
+  end
+
+  wt = Wavelets.wavelet(Wavelets.WT.haar)
+
+  # ----- Discrete Wavelet Transform (DWT) -----
+
+  # Function W: Applies the DWT to an input x
+  function W(x)
+    return Wavelets.dwt(reshape_array(x, (n, m)), wt, 4)[:]
+  end
+
+  # Function W_T: Applies the inverse DWT to an input x
+  function W_T(x)
+    return Wavelets.idwt(reshape_array(x, (n, m)), wt, 4)[:]
   end
 
   # Return the generated functions
--- src/denoising_model.jl
+++ src/denoising_model.jl
@@ -1,12 +1,16 @@
+using Images
+using FFTW
+using Wavelets
 export denoising_model
 
 include("denoising_data.jl")
 
-function denoing_model(shape, shape_p, KERNEL_SIZE, KERNEL_TYPE, KERNEL_SIGMA = 1.5)
+function denoising_model(shape, shape_p, KERNEL_SIZE, KERNEL_SIGMA = 1.5)
   sigma = 10^-3
-  cameraman_image = load("images/cameraman.png")
+  data_path = joinpath(@__DIR__, "..", "images/cameraman.png")
+  cameraman_image = Images.load(data_path)
   x_t = vec(Float64.(cameraman_image))
-  H, H_T, W, W_T = generate_HW(shape, shape_p, KERNEL_SIZE, KERNEL_TYPE, KERNEL_SIGMA)
+  H, H_T, W, W_T = generate_gaussian_blur(shape, shape_p, KERNEL_SIZE, KERNEL_SIGMA)
   (n, m) = shape
   b = H(x_t) + randn(n * m) * sigma
   y = similar(b)
@@ -15,15 +19,14 @@ function denoing_model(shape, shape_p, KERNEL_SIZE, KERNEL_TYPE, KERNEL_SIGMA = 
   function obj(x)
     y .= W_T(x)
     y .= H(y)
-    z .= log.((y - b) .^ 2 .+ 1)
+    z .= log.((y .- b) .^ 2 .+ 1)
     return sum(z)
   end
 
   function grad!(g, x)
-    y = H(W_T(x))
-    term = 1.0 ./ ((y - b) .^ 2 .+ 1)
-    z = term .* (y - b)
-    z .*= 2
+    y .= H(W_T(x))
+    z .= 1 ./ ((y .- b) .^ 2 .+ 1)
+    @. z = 2 * z * (y - b)
     g .= W(H_T(z))
     return g
   end
