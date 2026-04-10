940bc915749b3839be4363f33be6b312b5c98196
MaxenceGollier <mgollier45@gmail.com>
2026-04-10 14:40:33 -0400

    revert MLDatasets tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -110,7 +110,6 @@ end
   @test all(0 .<= nls_model.meta.x0 .<= 1)
 end
 
-"""
 @testset "SVM-Train" begin
   ENV["DATADEPS_ALWAYS_ACCEPT"] = true
   @test_throws ErrorException svm_train_model((1, 1))
@@ -146,7 +145,6 @@ end
   @test length(findall(x -> x .!= 1, sol)) == 1028
   @test length(findall(x -> x .!= -1, sol)) == 1135
 end
-"""
 
 @testset "NNMF" begin
   m, n, k = 100, 50, 10
